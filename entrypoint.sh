cc6f133abadec0c38bdff1a747597b47577ffd88
chrisfarms <chris@farmiloe.com>
2022-02-19 11:46:26 +0000

    setting pipelines with vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,21 @@
 
 set -eu
 
-fly -t self login -c "${CONCOURSE_URL}" -u "${CONCOURSE_USERNAME}" -p "${CONCOURSE_PASSWORD}" -n "${CONCOURSE_TEAM}"
-fly -t self "$@"
+fly -t self login \
+	-c "${CONCOURSE_URL}" \
+	-u "${CONCOURSE_USERNAME}" \
+	-p "${CONCOURSE_PASSWORD}" \
+	-n "${CONCOURSE_TEAM}"
+
+OPTIONAL_ARGS=""
+
+if [[ ! -z "${CONCOURSE_PIPELINE_VARS}" ]]; then
+	OPTIONAL_ARGS="-l ${GITHUB_WORKSPACE}/${CONCOURSE_PIPELINE_VARS}"
+fi
+
+fly -t self \
+	--non-interactive \
+	set-pipieline \
+	-p "${GITHUB_WORKSPACE}/${CONCOURSE_PIPELINE_NAME}" \
+	-c "${GITHUB_WORKSPACE}/${CONCOURSE_PIPELINE_CONFIG}" \
+	${OPTIONAL_ARGS}
